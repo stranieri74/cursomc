e80ff2a90ac5c0f55d252ea86cb4aa8db3369494
Stranieri74 <stranieri74@gmail.com>
2020-12-21 12:30:46 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -68,7 +68,11 @@ public class CursomcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+		Categoria cat3 = new Categoria(null, "Cama Mesa e Banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -105,7 +109,8 @@ public class CursomcApplication implements CommandLineRunner{
 	  
 	     
 	    
-	    categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	    categoriaRepository.saveAll(Arrays.asList(cat1, cat2,
+	    		cat3, cat4, cat5, cat6, cat7));
 	    produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	    estadoRepository.saveAll(Arrays.asList(est1, est2));
 	    cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -17,8 +17,8 @@ public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer Id;
-    private String Nome;
+    private Integer id;
+    private String nome;
     
     @ManyToMany(mappedBy="categorias")
     private List<Produto> produtos = new ArrayList<>();
@@ -27,15 +27,15 @@ public Categoria() {}
 
 public Categoria(Integer id, String nome) {
 	super();
-	Id = id;
-	Nome = nome;
+	this.id = id;
+	this.nome = nome;
 }
 
 @Override
 public int hashCode() {
 	final int prime = 31;
 	int result = 1;
-	result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
 	return result;
 }
 
@@ -48,28 +48,28 @@ public boolean equals(Object obj) {
 	if (getClass() != obj.getClass())
 		return false;
 	Categoria other = (Categoria) obj;
-	if (Id == null) {
-		if (other.Id != null)
+	if (id == null) {
+		if (other.id != null)
 			return false;
-	} else if (!Id.equals(other.Id))
+	} else if (!id.equals(other.id))
 		return false;
 	return true;
 }
 
 public Integer getId() {
-	return Id;
+	return id;
 }
 
 public void setId(Integer id) {
-	Id = id;
+	this.id = id;
 }
 
 public String getNome() {
-	return Nome;
+	return nome;
 }
 
 public void setNome(String nome) {
-	Nome = nome;
+	this.nome = nome;
 }
 
 public List<Produto> getProdutos() {
